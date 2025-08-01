53f75839f33f6014cfbe04ba4f85b9d337f3daab
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-01 17:48:25 +0000

    Enhance HTTP server to log requests to a file and update server port to 7000

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 //   res.writeHead(200, { 'Content-Type': 'text/plain' });
-   console.log("Request received");
+//  console.log(req.headers);
+//    console.log("Request received");
+fs.appendFile('log.txt', `Request received at ${new Date()} (${req.url})\n`, (err) => {
+    if (err) throw err;
+    console.log('Log updated');
+});
    res.end('Hello World\n');
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+server.listen(7000, () => {
+  console.log('Server running at http://localhost:7000');
 });
