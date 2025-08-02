93a8296fd9d8f5548a23739475ecb2062e8e7d3e
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-02 02:07:26 +0000

    Refactor request logging and error handling in HTTP server; add package-lock.json

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 const http = require('http');
 const fs = require('fs');
+const { URL } = require('url'); 
 
 const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//  console.log(req.headers);
-//    console.log("Request received");
-fs.appendFile('log.txt', `Request received at ${new Date()} (${req.url})\n`, (err) => {
-    //     if (err) throw err;
+    // Ignore favicon
+    if (req.url === '/favicon.ico') return res.end();
+
+    //  Define the URL object
+    const url = new URL(req.url, `http://${req.headers.host}`);
+
+    // Log request
+    fs.appendFile('log.txt', `Request received at ${new Date()}: (${req.url})\n`, (err) => {
+        if (err) {
+            console.error('Log write error:', err);
+            res.statusCode = 500;
+            return res.end('Internal Server Error');
+        }
+
         console.log('Log updated');
-    // });
-    //    res.end('Hello World\n');
-    switch (req.url) {
-        case '/':
-            res.end('Welcome to the Home Page\n');
-            break;
-        case '/about':
-            res.end('This is the About Page\n');
-            break;
-        case '/contact':
-            res.end('This is the Contact Page\n');
-            break;
-        default:
-            res.end('Page not found\n');
-            break;
-    }
+
+        //  Route handling based on pathname
+        switch (url.pathname) {
+            case '/':
+                res.end('Welcome to the Home Page\n');
+                break;
+            case '/about':
+                res.end('This is the About Page\n');
+                break;
+            case '/contact':
+                res.end('This is the Contact Page\n');
+                break;
+            default:
+                res.end('Page not found\n');
+                break;
+        }
     });
 });
 
-server.listen(7000, () => {
-  console.log('Server running at http://localhost:7000');
+
+server.listen(8000, () => {
+    console.log('Server running at http://localhost:8000');
 });
